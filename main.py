d0ac3bbf8e375d7c9f202fe529918447b2d0e6cb
R-andiika <riskiandika579@gmail.com>
2024-06-15 16:18:36 +0700

    Commit message

--- main.py
+++ main.py
@@ -1,24 +1,28 @@
-from fastapi import FastAPI, File, UploadFile, HTTPException
+from fastapi import FastAPI, File, UploadFile, Form, HTTPException
 from fastapi.responses import JSONResponse
 import tensorflow as tf
 from tensorflow.keras.preprocessing import image
 import numpy as np
 from io import BytesIO
-from google.cloud import storage, firestore
+from google.cloud import firestore
 import os
+import uvicorn
+from datetime import datetime
 
 app = FastAPI()
 
 class_names = ['Abnormal(Ulcer)', 'Normal(Healthy skin)', 'Wound Images']
 
 # Muat model
-model = tf.keras.models.load_model('/home/tartar/predict-api/model/model_fix.h5')
+model = tf.keras.models.load_model('model_fix.h5')
 
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "./config/serviceAccount.json"
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/secrets/serviceAccountKey.json"
 
 def predict_image(img_array):
     predictions = model.predict(img_array)
+    print(f"Raw predictions: {predictions}")
     score = tf.nn.softmax(predictions[0])
+    print(f"Softmax score: {score}")
     return {
         "class": class_names[np.argmax(score)],
         "confidence": float(100 * np.max(score))
@@ -29,7 +33,7 @@ def store_data_in_firestore(data):
     db.collection('predictions').add(data)
 
 @app.post("/predict")
-async def predict(file: UploadFile = File(...)):
+async def predict(file: UploadFile = File(...), userId: str = Form(...)):
     try:
         # Muat gambar
         img = image.load_img(BytesIO(await file.read()), target_size=(224, 224))
@@ -41,6 +45,11 @@ async def predict(file: UploadFile = File(...)):
         # Prediksi gambar
         predictions = predict_image(img_array)
 
+        # Tambahkan userId dan created_at ke dalam prediksi
+        created_at = datetime.now().isoformat()
+        predictions['userId'] = userId
+        predictions['created_at'] = created_at
+
         # Simpan hasil prediksi ke Firestore
         store_data_in_firestore(predictions)
 
@@ -50,4 +59,4 @@ async def predict(file: UploadFile = File(...)):
         raise HTTPException(status_code=500, detail=str(e))
 
 if __name__ == '__main__':
-    uvicorn.run(app, debug=True, host='0.0.0.0', port=14045)
+    uvicorn.run(app, debug=True, host='0.0.0.0', port=8080)
